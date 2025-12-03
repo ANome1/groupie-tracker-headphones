89eae9e9249f7ea45637a80477f11438b22a41b7
ANome1 <anomenafitia@gmail.com>
2025-12-03 17:46:04 +0100

    generate code

--- services/room_service.go
+++ services/room_service.go
@@ -1,3 +1,20 @@
 package services
 
 // TODO @Nome: CreateRoom, GetRoomByCode, JoinRoom, LeaveRoom, GenerateRoomCode
+import (
+	"groupie-tracker/database"
+	"math/rand"
+)
+
+type RoomService struct {
+	DB *database.Database
+}
+
+func GenerateRoomCode() string {
+	const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	code := make([]byte, 6)
+	for i := range code {
+		code[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(code)
+}
