c7d673c0b3c808255640daf38cfbc88029ce967b
ANome1 <anomenafitia@gmail.com>
2025-12-03 17:29:37 +0100

    check validation

--- handlers/auth.go
+++ handlers/auth.go
@@ -3,3 +3,30 @@ package handlers
 // TODO @Nome: Handler POST pour inscription (récupérer form, valider, hasher SHA256, insérer en DB)
 // TODO @Nome: Handler POST pour connexion (vérifier credentials, créer session)
 // TODO @Nome: Handler pour déconnexion
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"regexp"
+)
+
+func HashPassword(password string) string {
+	hash := sha256.Sum256([]byte(password))
+	return hex.EncodeToString(hash[:])
+}
+func CheckPasswordHash(password, hash string) bool {
+	return HashPassword(password) == hash
+}
+
+func ValidateEmail(email string) bool {
+	re := regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+	return re.MatchString(email)
+}
+
+func ValidateUsername(username string) bool {
+	re := regexp.MustCompile(`^[a-zA-Z0-9_]{3,20}$`)
+	return re.MatchString(username)
+}
+
+func ValidatePassword(password string) bool {
+	return len(password) >= 8
+}
