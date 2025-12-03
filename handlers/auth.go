366b0e523701fe5d28c8299739226ce071ff410a
ANome1 <anomenafitia@gmail.com>
2025-12-03 17:30:18 +0100

    check validation

--- handlers/auth.go
+++ handlers/auth.go
@@ -6,11 +6,13 @@ package handlers
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"log"
 	"regexp"
 )
 
 func HashPassword(password string) string {
 	hash := sha256.Sum256([]byte(password))
+	log.Printf("Hashed password: %x", hash)
 	return hex.EncodeToString(hash[:])
 }
 func CheckPasswordHash(password, hash string) bool {
@@ -19,14 +21,17 @@ func CheckPasswordHash(password, hash string) bool {
 
 func ValidateEmail(email string) bool {
 	re := regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+	log.Printf("Validating email: %s", email)
 	return re.MatchString(email)
 }
 
 func ValidateUsername(username string) bool {
 	re := regexp.MustCompile(`^[a-zA-Z0-9_]{3,20}$`)
+	log.Printf("Validating username: %s", username)
 	return re.MatchString(username)
 }
 
 func ValidatePassword(password string) bool {
+	log.Printf("Validating password length: %d", len(password))
 	return len(password) >= 8
 }
