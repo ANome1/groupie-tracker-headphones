0bb3b7daaecc051c30e5282fdd3a049a88fe44ab
ANome1 <anomenafitia@gmail.com>
2025-12-19 19:50:29 +0100

    security fix

--- server.go
+++ server.go
@@ -359,7 +359,7 @@ func main() {
 		w.Header().Set("X-Frame-Options", "DENY")
 		w.Header().Set("X-XSS-Protection", "1; mode=block")
 		w.Header().Set("Referrer-Policy", "strict-origin-when-cross-origin")
-		
+
 		// HSTS (Strict-Transport-Security) - activer en production avec HTTPS
 		// w.Header().Set("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload")
 	})
